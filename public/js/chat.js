885b1bc9fd971acbcb71846f26fb2df648821469
Pratik Goti <88868354+Pratik8418@users.noreply.github.com>
2023-04-15 14:28:07 +0530

    Form level testing and Acknowledgements done

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,12 +1,26 @@
 const socket = io()
 
+const $messageForm = document.querySelector('#message-form')
+const $messageSendButton = document.querySelector('#target')
+const $messageInput = document.querySelector('#input')
+const $sendLocationButton = document.querySelector('#send-location')
+
 socket.on('welcome', (msg) => {
   console.log(" ", msg);
 })
 
-document.querySelector('#target').addEventListener('click', () => {
-   var input = document.getElementById('input').value;
+$messageForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  $messageSendButton.setAttribute('disabled','disabled');
+    
+   const input = document.querySelector('#input').value;
    socket.emit('inputMsg', input , (error) => {
+
+    $messageSendButton.removeAttribute('disabled')
+    $messageInput.value = ''
+    $messageInput.focus()
+
     if(error){
       return console.log(error);
     }
@@ -16,6 +30,8 @@ document.querySelector('#target').addEventListener('click', () => {
 })
 
 document.querySelector("#send-location").addEventListener('click', () => {
+
+  $sendLocationButton.setAttribute('disabled','disabled')
 
   if(!navigator.geolocation){
     return alert("GeoLocation is not supported by your browser")
@@ -29,6 +45,7 @@ document.querySelector("#send-location").addEventListener('click', () => {
     //console.log(data.latitude + " " + data.longitude)
     socket.emit("location",data, () => {
       console.log("Location send successfully")
+      $sendLocationButton.removeAttribute('disabled')
     });
   
   })
