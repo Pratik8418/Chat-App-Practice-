43e900638a28a0b27ff9225b239b48c5ae612bfd
Pratik Goti <88868354+Pratik8418@users.noreply.github.com>
2023-04-15 17:55:31 +0530

    User added 2

--- src/utils/users.js
+++ src/utils/users.js
@@ -33,7 +33,23 @@ const removeUser = (id) => {
     return user.id === id 
   })
 
-  if(!index !== -1){
+  if(index !== -1){
     return users.splice(index,1)[0]
   }
+}
+
+const getUser = (id) =>{
+    return users.find( (user) => user.id === id)
+}
+
+const getUsersInRoom = (room) => {
+  room = room.trim().toLowerCase()
+  return users.filter( (user) => user.room === room)
+}
+
+module.exports = {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom
 }
